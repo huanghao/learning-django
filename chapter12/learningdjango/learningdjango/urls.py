edfce6c50354c7dea963371cf6dffa9ab34f046b
Huang Hao <hao.h.huang@intel.com>
2014-08-09 02:09:50 +0800

    add signup

--- chapter12/learningdjango/learningdjango/urls.py
+++ chapter12/learningdjango/learningdjango/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.shortcuts import render
+from django.shortcuts import resolve_url
+from django.http import HttpResponseRedirect
+from django.template.response import TemplateResponse
 
 from django.contrib import admin
 admin.autodiscover()
@@ -16,6 +19,21 @@ urlpatterns = patterns('',
 def profile(request):
     return render(request, 'profile.html')
 
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.info(request, "Please login with the account created")
+            return HttpResponseRedirect(resolve_url('login'))
+    else:
+        form = UserCreationForm()
+    context = {'form': form}
+    return TemplateResponse(request, 'signup.html', context)
+
 
 urlpatterns += patterns(
     'django.contrib.auth.views',
@@ -23,8 +41,11 @@ urlpatterns += patterns(
     url(r'^accounts/login/$', 'login', name='login'),
     url(r'^accounts/logout/$', 'logout', name='logout'),
     url(r'^accounts/profile/$', profile, name='profile'),
-    url(r'^accounts/password_change/$', 'password_change', name='password_change'),
-    url(r'^accounts/password_change/done/$', 'password_change_done', name="password_change_done"),
+    url(r'^accounts/password_change/$',
+        'password_change', name='password_change'),
+    url(r'^accounts/password_change/done/$',
+        'password_change_done', name="password_change_done"),
+    url(r'accounts/signup', signup, name="signup"),
 )
 
 
