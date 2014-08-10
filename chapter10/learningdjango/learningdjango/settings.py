9b68a32d32580de390bc911302b98c5daca2fbaa
Huang, Hao <huang1hao@gmail.com>
2014-08-10 22:39:56 +0800

    change mysql to sqlite3

--- chapter10/learningdjango/learningdjango/settings.py
+++ chapter10/learningdjango/learningdjango/settings.py
@@ -58,9 +58,8 @@ WSGI_APPLICATION = 'learningdjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'learningdjango',
-        'USER': 'root',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
