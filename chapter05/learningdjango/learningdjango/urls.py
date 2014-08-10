b3e10c6e015aa03f69f5cafb8df6cd1bbd138cf6
Huang Hao <hao.h.huang@intel.com>
2014-08-10 21:49:06 +0800

    remove useless code in chapter05

--- chapter05/learningdjango/learningdjango/urls.py
+++ chapter05/learningdjango/learningdjango/urls.py
@@ -9,8 +9,4 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-
-    url(r'^hello/$', 'polls.views.hello'),
-    url(r'^time/$', 'polls.views.current_datetime'),
-    url(r'^time/plus/(\d+)$', 'polls.views.hours_ahead'),
 )
